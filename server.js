5fba6366974d76974fc763f13615cecd5eeea526
TJ James <jamestw13@gmail.com>
2021-11-28 13:43:23 -0600

    setup mysql models

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
 app.use(routes);
 
